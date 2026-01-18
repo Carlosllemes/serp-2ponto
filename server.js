aab178472a9bc9829b19bcc9fb764feaeaf998a6
Carlos Alberto Lemes <39310873+Carlosllemes@users.noreply.github.com>
2026-01-18 10:07:38 -0300

    ajuste de porta

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { extractLinks } = require('./scraper');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 app.use(cors());
 app.use(express.json());
